f9fe959ca6c749957d86c90859cb8d93b20c51ae
BSC11426 <vinay.korasika@bcone.com>
2017-01-22 09:23:04 +0530

    first commit
    Change-Id: Ieaa03a68741a3d687aded8602f8eee39e8876208
    Signed-off-by: BSC11426 <vinay.korasika@bcone.com>

---
+++ test/src/test/Helper.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Helper {
+ 
+	public static void Help()
+	{
+		System.out.println("Hey im in helper");
+	}
+
+}
---
+++ test/src/test/Start.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Start {
+	public static void main(String[] args) {
+		System.out.println("hey im in main");
+		Helper hp=new Helper();
+		hp.Help();
+	}
+
+}
